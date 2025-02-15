d1065cec8d2f5660c2eb8e41f4150252ff07e525
Harshit451 <shardaharshit42@gmail.com>
2025-02-16 00:35:20 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,27 +1,51 @@
-#include<stdio.h>
-int main(){
-    int a,b;
+// #include<stdio.h>
+// int main(){
+//     int a,b;
+//     char sign;
+//     scanf("%d %d %c",&a,&b,&sign);
+//     if(sign== '+'){
+//         printf("%d",a + b);
+//     }
+//     else if(sign== '-'){
+//         printf("%d",a - b);
+//     }
+//     else if(sign== '*'){
+//         printf("%d",a * b);
+//     }
+//     else if(sign== '/'){
+//         if(b!=0){
+//             printf("%d",a / b);
+//             }
+//             else{
+//                 printf("error");
+//             }
+//     }
+//     // else{
+//     //     printf("error");
+//     // }
+//     return 0;
+// }
+
+
+
+// By Switch
+#include <stdio.h>
+int main() {
+    int a, b;
     char sign;
-    scanf("%d %d %c",&a,&b,&sign);
-    if(sign== '+'){
-        printf("%d",a + b);
-    }
-    else if(sign== '-'){
-        printf("%d",a - b);
-    }
-    else if(sign== '*'){
-        printf("%d",a * b);
-    }
-    else if(sign== '/'){
-        if(b!=0){
-            printf("%d",a / b);
-            }
-            else{
+    scanf("%d %d %c", &a, &b, &sign);
+    switch (sign) {
+        case '+':printf("%d", a + b);break;
+        case '-':printf("%d", a - b);break;
+        case '*':printf("%d", a * b);break;
+        case '/':
+        if (b != 0)
+        printf("%d", a / b);
+        else
                 printf("error");
-            }
+            break;
+        default:
+            printf("error"); // Handles invalid operators
     }
-    // else{
-    //     printf("Error");
-    // }
     return 0;
 }
