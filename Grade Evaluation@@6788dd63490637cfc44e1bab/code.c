8b8cb923f3ffbb9d496c4764e531217cf9f58251
Harshit451 <shardaharshit42@gmail.com>
2025-02-16 01:08:00 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -1,40 +1,40 @@
-// #include<stdio.h>
-// int main(){
-//     char ch;
-//     scanf("%c",&ch);
-//     switch(ch){
-//         case 'A': printf("Excellent\n"); break;
-//         case 'B': printf("Good\n"); break;
-//         case 'C': printf("Average\n"); break;
-//         case 'D': printf("Below Average\n"); break;
-//         case 'F': printf("Fail\n"); break;
-//         default: printf("Invalid grade\n");
-//     }
-//     return 0;
-// }
-
-// If-Else
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+int main(){
     char ch;
-    scanf("%c", &ch);
-    if (ch == 'A') {
-        printf("Excellent\n");
-    }
-    else if (ch == 'B') {
-        printf("Good\n");
-    }
-    else if (ch == 'C') {
-        printf("Average\n");
-    }
-    else if (ch == 'D') {
-        printf("Below Average\n");
-    }
-    else if (ch == 'F') {
-        printf("Fail\n");
-    }
-    else {
-        printf("Invalid grade\n");
+    scanf("%c",&ch);
+    switch(ch){
+        case 'A': printf("Excellent\n"); break;
+        case 'B': printf("Good\n"); break;
+        case 'C': printf("Average\n"); break;
+        case 'D': printf("Below Average\n"); break;
+        case 'F': printf("Fail\n"); break;
+        default: printf("Invalid grade\n");
     }
     return 0;
 }
+
+// If-Else
+// #include <stdio.h>
+// int main() {
+//     char ch;
+//     scanf("%c", &ch);
+//     if (ch == 'A') {
+//         printf("Excellent\n");
+//     }
+//     else if (ch == 'B') {
+//         printf("Good\n");
+//     }
+//     else if (ch == 'C') {
+//         printf("Average\n");
+//     }
+//     else if (ch == 'D') {
+//         printf("Below Average\n");
+//     }
+//     else if (ch == 'F') {
+//         printf("Fail\n");
+//     }
+//     else {
+//         printf("Invalid grade\n");
+//     }
+//     return 0;
+// }
