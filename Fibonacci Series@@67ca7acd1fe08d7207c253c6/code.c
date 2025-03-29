20869c2fa3bd9c00fe501534b5e8ca64bdfbbfb3
Harshit451 <shardaharshit42@gmail.com>
2025-03-29 22:10:32 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -2,5 +2,5 @@ int fibonacciSeries(int n){
     if(n==1 || n==2){
         return n-1;
     }
-    return fibonacciSeries(n+1) + fibonacciSeries(n+2);
+    return fibonacciSeries(n-1) + fibonacciSeries(n-2);
 }
